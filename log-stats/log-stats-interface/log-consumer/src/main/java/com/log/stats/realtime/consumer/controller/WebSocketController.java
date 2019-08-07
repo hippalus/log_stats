0357a68a82b749414009a8a4bbd399ceb3b998c3
hisler <habiphakan.isler@gmail.com>
2019-08-07 14:28:18 +0300

    [log-consumer] web socket clint implemented

--- log-stats/log-stats-interface/log-consumer/src/main/java/com/log/stats/realtime/consumer/controller/WebSocketController.java
+++ log-stats/log-stats-interface/log-consumer/src/main/java/com/log/stats/realtime/consumer/controller/WebSocketController.java
@@ -1,4 +1,16 @@
 package com.log.stats.realtime.consumer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class WebSocketController {
+
+    @GetMapping("/index")
+    public String home(Model model) {
+        return "index";
+    }
 }
