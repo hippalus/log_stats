5ca3d852b5909da2bffd159caf31b29878b33bb6
hisler <habiphakan.isler@gmail.com>
2019-08-09 16:09:43 +0300

    [Log-FLow] chaced docker file

--- log-stats/log-flow/src/main/java/com/log/stats/logflow/LogFlowApplication.java
+++ log-stats/log-flow/src/main/java/com/log/stats/logflow/LogFlowApplication.java
@@ -15,49 +15,57 @@ import java.util.Properties;
 
 public class LogFlowApplication {
     public static void main(String[] args) throws Exception {
-
-        Properties kafkaBoltProps = new Properties();
-        kafkaBoltProps.put("bootstrap.servers", "127.0.0.1:9092");
-        kafkaBoltProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaBoltProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaBoltProps.put("batch.size", "0");
-        kafkaBoltProps.put("acks", "all");
-
-
-        LogFlowKafkaSpout logFlowKafkaSpout = new LogFlowKafkaSpout("127.0.0.1:9092", "cityLog-raw", "raw-log-consumer-group", 3000, 1000000);
-        LogRowValidatorBolt validatorBolt = new LogRowValidatorBolt();
-        ElasticSearchBolt elasticSearchBolt=new ElasticSearchBolt("city-logs" ,"valid","127.0.0.1",9300);
-
-        FieldNameBasedTupleToKafkaMapper<String, String> validLogRowMapper = new FieldNameBasedTupleToKafkaMapper<>("key", "valid-log");
-
-        KafkaBolt<String, String> kafkaValidBolt = new KafkaBolt<String, String>()
-                .withProducerProperties(kafkaBoltProps)
-                .withTopicSelector(new DefaultTopicSelector("cityLog-valid"))
-                .withTupleToKafkaMapper(validLogRowMapper);
-
-        FieldNameBasedTupleToKafkaMapper<String, String> errorMapper = new FieldNameBasedTupleToKafkaMapper<>("key", "invalid-log");
-
-        KafkaBolt<String, String> kafkaInvalidBolt = new KafkaBolt<String, String>()
-                .withProducerProperties(kafkaBoltProps)
-                .withTopicSelector(new DefaultTopicSelector("cityLog-invalid"))
-                .withTupleToKafkaMapper(errorMapper);
+        String KAFKA_BROKER = "127.0.0.1:9092";
+        String ELK_HOST = "127.0.0.1";
+        try {
+            KAFKA_BROKER = args[0];
+            ELK_HOST = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
 
+            Properties kafkaBoltProps = new Properties();
+            kafkaBoltProps.put("bootstrap.servers", KAFKA_BROKER);
+            kafkaBoltProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            kafkaBoltProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            kafkaBoltProps.put("batch.size", "0");
+            kafkaBoltProps.put("acks", "all");
 
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("kafkaSpout", logFlowKafkaSpout, 1);
-        builder.setBolt("validatorBolt", validatorBolt, 1).allGrouping("kafkaSpout");
-        builder.setBolt("kafkaValidBolt", kafkaValidBolt, 1).allGrouping("validatorBolt", "valid-log-stream");
-        builder.setBolt("kafkaInvalidBolt", kafkaInvalidBolt, 1).allGrouping("validatorBolt","invalid-log-stream");
-        builder.setBolt("elasticSearchBolt",elasticSearchBolt,1).shuffleGrouping("validatorBolt","valid-log-stream");
 
-        Config config = new Config();
-        config.setDebug(true);
-        config.setNumWorkers(1);
-        config.setMaxSpoutPending(5000);
+            LogFlowKafkaSpout logFlowKafkaSpout = new LogFlowKafkaSpout(KAFKA_BROKER, "cityLog-raw", "raw-log-consumer-group", 3000, 1000000);
+            LogRowValidatorBolt validatorBolt = new LogRowValidatorBolt();
+            ElasticSearchBolt elasticSearchBolt = new ElasticSearchBolt("city-logs", "valid", ELK_HOST, 9300);
 
-       // LocalCluster local = new LocalCluster();
-        StormSubmitter.submitTopology("log_flow_topology", config, builder.createTopology());
+            FieldNameBasedTupleToKafkaMapper<String, String> validLogRowMapper = new FieldNameBasedTupleToKafkaMapper<>("key", "valid-log");
+
+            KafkaBolt<String, String> kafkaValidBolt = new KafkaBolt<String, String>()
+                    .withProducerProperties(kafkaBoltProps)
+                    .withTopicSelector(new DefaultTopicSelector("cityLog-valid"))
+                    .withTupleToKafkaMapper(validLogRowMapper);
+
+            FieldNameBasedTupleToKafkaMapper<String, String> errorMapper = new FieldNameBasedTupleToKafkaMapper<>("key", "invalid-log");
+
+            KafkaBolt<String, String> kafkaInvalidBolt = new KafkaBolt<String, String>()
+                    .withProducerProperties(kafkaBoltProps)
+                    .withTopicSelector(new DefaultTopicSelector("cityLog-invalid"))
+                    .withTupleToKafkaMapper(errorMapper);
+
+
+            TopologyBuilder builder = new TopologyBuilder();
+            builder.setSpout("kafkaSpout", logFlowKafkaSpout, 1);
+            builder.setBolt("validatorBolt", validatorBolt, 1).allGrouping("kafkaSpout");
+            builder.setBolt("kafkaValidBolt", kafkaValidBolt, 1).allGrouping("validatorBolt", "valid-log-stream");
+            builder.setBolt("kafkaInvalidBolt", kafkaInvalidBolt, 1).allGrouping("validatorBolt", "invalid-log-stream");
+            builder.setBolt("elasticSearchBolt", elasticSearchBolt, 1).shuffleGrouping("validatorBolt", "valid-log-stream");
+
+            Config config = new Config();
+            config.setDebug(true);
+            config.setNumWorkers(1);
+            config.setMaxSpoutPending(5000);
+
+            // LocalCluster local = new LocalCluster();
+            StormSubmitter.submitTopology("log_flow_topology", config, builder.createTopology());
 
     }
 }
